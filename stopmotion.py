4a8c145f9eac5cb5f32f305d8ed893ae711f7b3a
jpresern <janez.presern@guest.arnes.si>
2021-11-26 17:23:24 +0100

    initial

--- stopmotion.py
+++ stopmotion.py
@@ -8,21 +8,22 @@
 import cv2
 import numpy as np
 from time import sleep
-from Tkinter import *
+from tkinter import *
 from imutils.video import WebcamVideoStream # https://github.com/jrosebr1/imutils
 import imutils
+import sys
 #from threading import Thread
-import RPi.GPIO as GPIO
+# import RPi.GPIO as GPIO
 
 #initializing GPIO pins
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(26,GPIO.OUT)
-GPIO.setup(6,GPIO.IN,GPIO.PUD_DOWN) #capture
-GPIO.setup(5,GPIO.IN,GPIO.PUD_DOWN) #play
-GPIO.setup(4,GPIO.IN,GPIO.PUD_DOWN) #reset
-GPIO.setup(17,GPIO.IN,GPIO.PUD_DOWN) #undo
-GPIO.output(26,GPIO.HIGH)
+# GPIO.setwarnings(False)
+# GPIO.setmode(GPIO.BCM)
+# GPIO.setup(26,GPIO.OUT)
+# GPIO.setup(6,GPIO.IN,GPIO.PUD_DOWN) #capture
+# GPIO.setup(5,GPIO.IN,GPIO.PUD_DOWN) #play
+# GPIO.setup(4,GPIO.IN,GPIO.PUD_DOWN) #reset
+# GPIO.setup(17,GPIO.IN,GPIO.PUD_DOWN) #undo
+# GPIO.output(26,GPIO.HIGH)
 
 #initializing some variables
 AnimFrameRate = 10
@@ -41,26 +42,26 @@ font = cv2.FONT_HERSHEY_SIMPLEX
 root = Tk()
 screen_width = root.winfo_screenwidth()
 screen_height = root.winfo_screenheight()
-print "Sua resolucao: %s x %s" %(screen_width, screen_height)
+print("Resolution: %s x %s" %(screen_width, screen_height))
 
 vs = WebcamVideoStream(src=0).start() #start threading
 
 cv2.namedWindow("video", cv2.WND_PROP_FULLSCREEN)
 cv2.setWindowProperty("video", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
 
-print "Inicializando Camera..." #Initializing Camera
+print("initializing camera...") #Initializing Camera
 sleep(0.5)
-print "Ajustando exposicao..." #Adjusting Exposition - Otherwise camera
+print("adjusting expositon...") #Adjusting Exposition - Otherwise camera
 #could start darker
 sleep(0.5)
-GPIO.output(26,GPIO.LOW)
-print "Running..."
+# GPIO.output(26,GPIO.LOW)
+print("Running...")
 
 try:
     vid_height, vid_width = vs.read().shape[:2]
 except:
-    print "Camera nao encontrada. Reconecte a camera e tente novamente."
-    sys.exit();
+    print("Camera nao encontrada. Reconecte a camera e tente novamente.")
+    sys.exit()
 
 icon_width = int(vid_width*screen_height*0.1/vid_height)
 icon_height = int(screen_height*0.1)
@@ -70,14 +71,15 @@ black = np.zeros((screen_height,screen_width,3), np.uint8)
 overlay = np.zeros((vid_height,vid_width,3), np.uint8)
 
 #layout drawing function
+
 def layout():
     for i in range(0,9):
         cv2.rectangle(black,(i*icon_width,int(screen_height*0.9)),(icon_width + i*icon_width,screen_height),(255,255,255),3)
-    cv2.putText(black,'Stop Motion',(int(vid_width + (screen_width - vid_width)*0.05),int((screen_width - vid_width)*0.12)), font, (screen_width - vid_width)/210.0,(0,0,255), screen_width/480,cv2.LINE_AA)
-    cv2.putText(black,'MACHINE',(int(vid_width + (screen_width - vid_width)*0.05),int((screen_width - vid_width)*0.28)), font, (screen_width - vid_width)/150.0,(0,0,255),screen_width/720,cv2.LINE_AA)
-    cv2.putText(black,'Taxa:',(int(vid_width + (screen_width - vid_width)*0.1),int((screen_width - vid_width)*0.55)), font, (screen_width - vid_width)/210.0,(255,255,255), screen_width/480,cv2.LINE_AA)
-    cv2.putText(black,str(AnimFrameRate),(int(vid_width + (screen_width - vid_width)*0.1),int((screen_width - vid_width)*0.7)), font, (screen_width - vid_width)/210.0,(0,255,0), screen_width/480,cv2.LINE_AA)
-    cv2.putText(black,'FPS',(int(vid_width + (screen_width - vid_width)*0.3),int((screen_width - vid_width)*0.7)), font, (screen_width - vid_width)/210.0,(255,255,255), screen_width/480,cv2.LINE_AA)
+    cv2.putText(black,'Stop Motion',(int(vid_width + (screen_width - vid_width)*0.05),int((screen_width - vid_width)*0.12)), font, int((screen_width - vid_width)/210.0),(0,0,255), int(screen_width/480),cv2.LINE_AA)
+    cv2.putText(black,'MACHINE',(int(vid_width + (screen_width - vid_width)*0.05),int((screen_width - vid_width)*0.28)), font, int((screen_width - vid_width)/150.0),(0,0,255), int(screen_width/720),cv2.LINE_AA)
+    cv2.putText(black,'Taxa:',(int(vid_width + (screen_width - vid_width)*0.1),int((screen_width - vid_width)*0.55)), font, int((screen_width - vid_width)/210.0),(255,255,255), int(screen_width/480),cv2.LINE_AA)
+    cv2.putText(black,str(AnimFrameRate),(int(vid_width + (screen_width - vid_width)*0.1),int((screen_width - vid_width)*0.7)), font, int((screen_width - vid_width)/210.0),(0,255,0), int(screen_width/480),cv2.LINE_AA)
+    cv2.putText(black,'FPS',(int(vid_width + (screen_width - vid_width)*0.3),int((screen_width - vid_width)*0.7)), font, int((screen_width - vid_width)/210.0),(255,255,255), int(screen_width/480),cv2.LINE_AA)
 
 #capture function
 def cap():
@@ -95,24 +97,24 @@ def cap():
     ovlay()
 
 def ovlay():
-	global overlay
-	global opacity
-	global actSeqFrame
-	global seq
-	overlay = np.zeros((vid_height,vid_width,3), np.uint8)
-	if (actSeqFrame == 0):
-		opacity = 0
-	if (actSeqFrame == 1):
-		overlay = seq[actSeqFrame-1]
-	if (actSeqFrame == 2):
-		cv2.addWeighted(seq[actSeqFrame-2], 0.5, seq[actSeqFrame-1], 0.5, 0, overlay)
-	if (actSeqFrame == 3):
-		cv2.addWeighted(seq[actSeqFrame-3], 0.5, seq[actSeqFrame-2], 0.5, 0, overlay)
-		cv2.addWeighted(overlay, 0.5, seq[actSeqFrame-1], 0.5, 0, overlay)
-	if (actSeqFrame >= 4):
-		cv2.addWeighted(seq[actSeqFrame-4], 0.5, seq[actSeqFrame-3], 0.5, 0, overlay)
-		cv2.addWeighted(overlay, 0.5, seq[actSeqFrame-2],0.5, 0, overlay)
-		cv2.addWeighted(overlay, 0.5, seq[actSeqFrame-1], 0.5, 0, overlay)
+    global overlay
+    global opacity
+    global actSeqFrame
+    global seq
+    overlay = np.zeros((vid_height,vid_width,3), np.uint8)
+    if (actSeqFrame == 0):
+        opacity = 0
+    if (actSeqFrame == 1):
+        overlay = seq[actSeqFrame-1]
+    if (actSeqFrame == 2):
+        cv2.addWeighted(seq[actSeqFrame-2], 0.5, seq[actSeqFrame-1], 0.5, 0, overlay)
+    if (actSeqFrame == 3):
+        cv2.addWeighted(seq[actSeqFrame-3], 0.5, seq[actSeqFrame-2], 0.5, 0, overlay)
+        cv2.addWeighted(overlay, 0.5, seq[actSeqFrame-1], 0.5, 0, overlay)
+    if (actSeqFrame >= 4):
+        cv2.addWeighted(seq[actSeqFrame-4], 0.5, seq[actSeqFrame-3], 0.5, 0, overlay)
+        cv2.addWeighted(overlay, 0.5, seq[actSeqFrame-2],0.5, 0, overlay)
+        cv2.addWeighted(overlay, 0.5, seq[actSeqFrame-1], 0.5, 0, overlay)
 
 
 def play():
@@ -121,12 +123,12 @@ def play():
     for i in range(0, actSeqFrame):
         black[0:vid_height, 0:vid_width] = seq[i]
         cv2.imshow('video', black)
-        key = cv2.waitKey(1000/AnimFrameRate) & 0xFF
-        if (key == ord('r')) or GPIO.input(4):
+        key = cv2.waitKey(int(1000/AnimFrameRate)) & 0xFF
+        if (key == ord('r')):# or GPIO.input(4):
             reset()
             return
         else:
-            if (key == ord('c')) or GPIO.input(6):
+            if (key == ord('c')):# or GPIO.input(6):
                 return
             else:
                 fRate()
@@ -172,7 +174,7 @@ while key!= ord('q'):
 
     key = cv2.waitKey(1) & 0xFF #keys are listened at this line
     
-    if (key == ord('c')) or GPIO.input(6):
+    if (key == ord('c')):# or GPIO.input(6):
         for i in range(0,9):
             cv2.rectangle(black,(i*icon_width,int(screen_height*0.9)),(icon_width + i*icon_width,screen_height),(255,255,255),3)
         cap()
@@ -182,16 +184,16 @@ while key!= ord('q'):
         if actIcon == 9:
             actIcon = 0
     else:
-        if (key == ord('p')) or GPIO.input(5):
+        if (key == ord('p')):# or GPIO.input(5):
             if actSeqFrame > 0:
                 play()
             else:
-                print "Nao existem quadros para a sequencia!"
+                print("Nao existem quadros para a sequencia!")
         else:
-            if (key == ord('r')) or GPIO.input(4):
+            if (key == ord('r')):# or GPIO.input(4):
                 reset()
             else:
-                if (key == ord('a')) or GPIO.input(17):
+                if (key == ord('a')):# or GPIO.input(17):
                     if actSeqFrame > 0:
                         actSeqFrame -= 1
                     if actSeqIcon > 0:
@@ -210,10 +212,10 @@ while key!= ord('q'):
                             cv2.rectangle(black,(actIcon*icon_width,int(screen_height*0.9)),(icon_width + actIcon*icon_width,screen_height),(0,0,0),-1)
                             cv2.rectangle(black,(actIcon*icon_width,int(screen_height*0.9)),(icon_width + actIcon*icon_width,screen_height),(255,255,255),3)
                             cv2.rectangle(black,((actIcon-1)*icon_width,int(screen_height*0.9)),(icon_width + (actIcon-1)*icon_width,screen_height),(0,255,0),3)
-                	ovlay()
+                    ovlay()
                 else:
                     fRate()
 
 cv2.destroyAllWindows()
 vs.stop()
-print "Programa finalizado."
+print("Programa finalizado.")
